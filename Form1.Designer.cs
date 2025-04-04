42ac592db04a9b2798dd6a49e8596814d2d7252c
Berkan Gülyağcı <bgulyaci@gmail.com>
2025-04-04 17:32:13 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace UAVLogViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea10 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend10 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnFilterData = new System.Windows.Forms.Button();
+            this.txtMaxValue = new System.Windows.Forms.TextBox();
+            this.txtMinValue = new System.Windows.Forms.TextBox();
+            this.btnCalculateStatistics = new System.Windows.Forms.Button();
+            this.btnNormalizeData = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(999, 546);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(111, 35);
+            this.btnLoadFile.TabIndex = 0;
+            this.btnLoadFile.Text = "btnLoadFile";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(833, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(277, 485);
+            this.listBox1.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(828, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(282, 15);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea10.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea10);
+            legend10.Name = "Legend1";
+            this.chart1.Legends.Add(legend10);
+            this.chart1.Location = new System.Drawing.Point(83, 37);
+            this.chart1.Name = "chart1";
+            series10.ChartArea = "ChartArea1";
+            series10.Legend = "Legend1";
+            series10.Name = "Series1";
+            this.chart1.Series.Add(series10);
+            this.chart1.Size = new System.Drawing.Size(487, 556);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // btnFilterData
+            // 
+            this.btnFilterData.Location = new System.Drawing.Point(707, 232);
+            this.btnFilterData.Name = "btnFilterData";
+            this.btnFilterData.Size = new System.Drawing.Size(101, 35);
+            this.btnFilterData.TabIndex = 4;
+            this.btnFilterData.Text = "btnFilterData";
+            this.btnFilterData.UseVisualStyleBackColor = true;
+            this.btnFilterData.Click += new System.EventHandler(this.btnFilterData_Click);
+            // 
+            // txtMaxValue
+            // 
+            this.txtMaxValue.Location = new System.Drawing.Point(707, 180);
+            this.txtMaxValue.Name = "txtMaxValue";
+            this.txtMaxValue.Size = new System.Drawing.Size(101, 20);
+            this.txtMaxValue.TabIndex = 5;
+            // 
+            // txtMinValue
+            // 
+            this.txtMinValue.Location = new System.Drawing.Point(707, 206);
+            this.txtMinValue.Name = "txtMinValue";
+            this.txtMinValue.Size = new System.Drawing.Size(101, 20);
+            this.txtMinValue.TabIndex = 6;
+            // 
+            // btnCalculateStatistics
+            // 
+            this.btnCalculateStatistics.Location = new System.Drawing.Point(833, 645);
+            this.btnCalculateStatistics.Name = "btnCalculateStatistics";
+            this.btnCalculateStatistics.Size = new System.Drawing.Size(131, 26);
+            this.btnCalculateStatistics.TabIndex = 7;
+            this.btnCalculateStatistics.Text = "btnCalculateStatistics";
+            this.btnCalculateStatistics.UseVisualStyleBackColor = true;
+            // 
+            // btnNormalizeData
+            // 
+            this.btnNormalizeData.Location = new System.Drawing.Point(999, 649);
+            this.btnNormalizeData.Name = "btnNormalizeData";
+            this.btnNormalizeData.Size = new System.Drawing.Size(131, 22);
+            this.btnNormalizeData.TabIndex = 8;
+            this.btnNormalizeData.Text = "btnNormalizeData";
+            this.btnNormalizeData.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(629, 183);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "max value";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(632, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "min value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1197, 707);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNormalizeData);
+            this.Controls.Add(this.btnCalculateStatistics);
+            this.Controls.Add(this.txtMinValue);
+            this.Controls.Add(this.txtMaxValue);
+            this.Controls.Add(this.btnFilterData);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnLoadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnFilterData;
+        private System.Windows.Forms.TextBox txtMaxValue;
+        private System.Windows.Forms.TextBox txtMinValue;
+        private System.Windows.Forms.Button btnCalculateStatistics;
+        private System.Windows.Forms.Button btnNormalizeData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET.WindowsForms;
+using GMap.NET;
+
+namespace UAVLogViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // GMapControl oluşturuluyor ve özellikler ayarlanıyor
+            this.gMapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+
+            // gMapControl
+            this.gMapControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            // Bu satırı bırakın:
+            this.gMapControl.MinZoom = 0;
+            this.gMapControl.MaxZoom = 24;
+            this.gMapControl.Location = new System.Drawing.Point(0, 0);
+            this.gMapControl.Margin = new System.Windows.Forms.Padding(4);
+            this.gMapControl.Name = "gMapControl";
+            this.gMapControl.Size = new System.Drawing.Size(800, 600);
+            this.gMapControl.TabIndex = 0;
+            this.gMapControl.MapProvider = GMap.NET.MapProviders.GoogleMapProvider.Instance; // Google haritası
+            this.gMapControl.MinZoom = 0;
+            this.gMapControl.MaxZoom = 24;
+            this.gMapControl.Zoom = 10;
+
+            // Form1
+            this.Controls.Add(this.gMapControl);
+            this.Name = "Form1";
+            this.Text = "UAV Log Viewer";
+            this.ResumeLayout(false);
+
+            // Arka planda yapılacak işlemleri tanımlıyoruz
+            backgroundWorker.DoWork += backgroundWorker_DoWork;
+            backgroundWorker.RunWorkerCompleted += backgroundWorker_RunWorkerCompleted;
+            backgroundWorker.ProgressChanged += backgroundWorker_ProgressChanged;
+
+            // ProgressBar'ı güncellemeye izin veriyoruz
+            backgroundWorker.WorkerReportsProgress = true;
+        }
+
+        private BackgroundWorker backgroundWorker = new BackgroundWorker();
+        private List<int> dataList = new List<int>(); // Sınıf düzeyinde veri listesi
+        private GMap.NET.WindowsForms.GMapControl gMapControl; // GMapControl için alan
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker.IsBusy)
+            {
+                MessageBox.Show("The previous operation is still running. Please wait for it to complete.");
+                return; // Eğer işlemi başlatmaya çalışıyorsanız, mevcut işlem devam ediyorsa yeni işlem başlatılmayacak
+            }
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Binary files (*.bin)|*.bin|All files (*.*)|*.*";
+            openFileDialog.Title = "Select a .bin File";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                MessageBox.Show($"File Selected: {filePath}");
+
+                // Arka planda işlemi başlatıyoruz
+                backgroundWorker.RunWorkerAsync(filePath);  // Dosya yolunu gönderiyoruz
+            }
+        }
+
+        // BackgroundWorker'ın DoWork metodunu kullanarak işlemi başlatıyoruz
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string filePath = (string)e.Argument;
+            List<int> tempDataList = new List<int>();
+
+            try
+            {
+                using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    using (BinaryReader reader = new BinaryReader(fs))
+                    {
+                        long totalLength = reader.BaseStream.Length;
+                        long currentPosition = 0;
+                        long maxRecords = 10000000000000000; // Okunacak maksimum veri sayısı
+
+                        while (reader.BaseStream.Position < totalLength && tempDataList.Count < maxRecords)
+                        {
+                            int data = reader.ReadInt32(); // Örneğin her veriyi int32 olarak okuyoruz
+                            tempDataList.Add(data);
+
+                            // İlerlemeyi raporluyoruz
+                            currentPosition = reader.BaseStream.Position;
+                            int progressPercentage = (int)((currentPosition * 100) / totalLength);
+                            backgroundWorker.ReportProgress(progressPercentage);
+                        }
+                    }
+                }
+
+                e.Result = tempDataList; // Okunan veriyi sonuç olarak gönderiyoruz
+            }
+            catch (Exception ex)
+            {
+                e.Result = "Error: " + ex.Message; // Hata mesajı gönder
+            }
+        }
+
+        // Arka planda işlemi tamamladıktan sonra bu metodu çalıştırıyoruz
+        private void backgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                MessageBox.Show("Error: " + e.Error.Message);
+            }
+            else
+            {
+                if (e.Result is List<int> tempDataList)
+                {
+                    dataList = tempDataList;
+
+                    // Burada GPS verilerinizi ayrıştırmanız gerekebilir.
+                    // GPS noktalarını PointLatLng formatına dönüştürün.
+                    List<GMap.NET.PointLatLng> gpsPoints = new List<GMap.NET.PointLatLng>();
+
+                    // Örnek: GPS verilerini listeye ekleyelim (latitude, longitude)
+                    gpsPoints.Add(new GMap.NET.PointLatLng(40.712776, -74.005974)); // Örnek: New York
+                    gpsPoints.Add(new GMap.NET.PointLatLng(34.052235, -118.243683)); // Örnek: Los Angeles
+
+                    // GPS noktalarını harita üzerinde çiziyoruz
+                    DrawFlightRoute(gpsPoints);
+
+                    MessageBox.Show("File processing completed successfully!");
+                }
+                else
+                {
+                    MessageBox.Show("File processing failed!");
+                }
+            }
+
+            progressBar1.Value = 0;
+        }
+
+        // ProgressChanged ile ilerleme yüzdesini güncelleme
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            // ProgressBar'ı güncelliyoruz
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        // Filtreleme işlemi
+        private void btnFilterData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int minValue = int.Parse(txtMinValue.Text);
+                int maxValue = int.Parse(txtMaxValue.Text);
+
+                // Filtrelenmiş verileri yeni bir listeye alalım
+                List<int> filteredData = dataList.Where(x => x >= minValue && x <= maxValue).ToList();
+
+                // ListBox'ı temizliyoruz ve filtrelenmiş verileri ekliyoruz
+                listBox1.Items.Clear();
+                foreach (var data in filteredData)
+                {
+                    listBox1.Items.Add(data);
+                }
+
+                // Chart'ı temizleyip, filtrelenmiş verileri ekliyoruz
+                chart1.Series[0].Points.Clear();
+                chart1.Series[0].Name = "Filtered Data"; // Filtreli veriler
+                foreach (var data in filteredData)
+                {
+                    chart1.Series[0].Points.AddY(data);
+                }
+
+                MessageBox.Show("Data filtered successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error filtering data: {ex.Message}");
+            }
+        }
+
+        private void DrawFlightRoute(List<GMap.NET.PointLatLng> points)
+        {
+            // Harita üzerinde yol çizmek için poligon ekliyoruz
+            GMap.NET.WindowsForms.GMapPolygon routePolygon = new GMap.NET.WindowsForms.GMapPolygon(points, "FlightRoute");
+            GMap.NET.WindowsForms.GMapOverlay routeOverlay = new GMap.NET.WindowsForms.GMapOverlay("Route");
+            routeOverlay.Polygons.Add(routePolygon);
+            gMapControl.Overlays.Add(routeOverlay);
+        }
+    }
+}
